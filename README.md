0426e0c76ff03a0c0be11df381c779555086ef4b
Harshitha-CB <harshithaneygar@gmail.com>
2022-06-07 02:13:30 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyTestingLibrary
+
+A description of this package.
---
+++ Tests/MyTestingLibraryTests/MyTestingLibraryTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import MyTestingLibrary
+
+final class MyTestingLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MyTestingLibrary().text, "Hello, World!")
+        
+        var std = Student(name: "john", roll: 1, marks: 100)
+        std.getPercentage()
+        std.name = "merry"
+    }
+}
